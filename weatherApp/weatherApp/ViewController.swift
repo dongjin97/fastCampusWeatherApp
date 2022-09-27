303c146f4b4025b8251009f13957c01ba593bfe6
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2022-09-27 12:44:06 +0900

    star Dev

---
+++ weatherApp/weatherApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by 원동진 on 2022/09/27.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
